fe84831ab44c3285f437910f4fd0c2cee606be1c
Michael Gallien <39108801+Mical15@users.noreply.github.com>
2023-03-01 19:54:06 -0800

    Cleaned up packages

--- ASTR TESTING/testing.js
+++ ASTR TESTING/testing.js
@@ -1,5 +1,7 @@
 const Astronomy = require('/Users/michaelgallien/Projects/astrLocations/The-Sky-Tonight/astronomy.js');
 
+// import {Observer, SearchRiseSet} from '/Users/michaelgallien/Projects/astrLocations/The-Sky-Tonight/astronomy.js';
+
 
 const location = {
   lat: 37.5355,
@@ -11,32 +13,33 @@ let yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1)
 const date = new Date();
 
-let sunSetTime = Astronomy.SearchRiseSet('Sun', obs, -1, date, 300).date.toLocaleString("en-US", {
+let sunSetTime = Astronomy.SearchRiseSet('Sun', obs, +1, date, 300).date.toLocaleString("en-US", {
   timeZone: "America/Los_Angeles"
 })
-let sunRiseTime = Astronomy.SearchRiseSet('Sun', obs, +1, date, 300).date.toLocaleString("en-US", {
+let sunRiseTime = Astronomy.SearchRiseSet('Sun', obs, -1, date, 300).date.toLocaleString("en-US", {
   timeZone: "America/Los_Angeles"
 })
 
-let sunSetDate = new Date(sunSetTime);
-let sunRiseDate = new Date(sunRiseTime);
 
 let planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Moon'];
 
 let riseTimes = planets.map(planet => {
-  return planet + ' ' + 'rises at' +  ' ' + Astronomy.SearchRiseSet(planet, obs, +1, yesterday, 300).date.toLocaleString("en-US", {
+  return Astronomy.SearchRiseSet(planet, obs, +1, yesterday, 300).date.toLocaleString("en-US", {
     timeZone: "America/Los_Angeles"
   })
 })
 
 let setTimes = planets.map(planet => {
-  return planet + ' ' + 'sets at' + Astronomy.SearchRiseSet(planet, obs, -1, date, 300).date.toLocaleString("en-US", {
+  return Astronomy.SearchRiseSet(planet, obs, -1, date, 300).date.toLocaleString("en-US", {
     timeZone: "America/Los_Angeles"
   })
 })
 
-// riseTimes.forEach(ele => console.log(ele))
-// setTimes.forEach(ele => console.log(ele))
-// console.log(sunSetTime);
 
-console.log(typeof sunRiseTime)
+riseTimes.forEach(ele => console.log(ele))
+console.log('-------------------')
+setTimes.forEach(ele => console.log(ele))
+console.log('sun rise time' + ' ' + sunSetTime);
+console.log('sun set time' + ' ' + sunRiseTime)
+
+console.log(riseTimes[0] < sunSetTime);
