53edcf4643cc8d617a5a062630f453ec343deea0
Michael Gallien <39108801+Mical15@users.noreply.github.com>
2023-03-08 19:47:46 -0800

    testing

--- ASTR TESTING/testing.js
+++ ASTR TESTING/testing.js
@@ -10,36 +10,82 @@ const location = {
 
 const obs = new Astronomy.Observer(location.lat, location.lon, 300);
 let yesterday = new Date();
-yesterday.setDate(yesterday.getDate() - 1)
+yesterday.setDate(yesterday.getDate() - 1);
 const date = new Date();
 
-let sunSetTime = Astronomy.SearchRiseSet('Sun', obs, +1, date, 300).date.toLocaleString("en-US", {
-  timeZone: "America/Los_Angeles"
-})
-let sunRiseTime = Astronomy.SearchRiseSet('Sun', obs, -1, date, 300).date.toLocaleString("en-US", {
-  timeZone: "America/Los_Angeles"
-})
+// .toLocaleString("en-US", {
+//   timeZone: "America/Los_Angeles"
+// })
+
+let test = Astronomy.SearchRiseSet('Sun', obs, +1, date, 300);
+
+let sunRiseTime = new Date(Astronomy.SearchRiseSet('Sun', obs, +1, date, 300))
+let sunSetTime = new Date(Astronomy.SearchRiseSet('Sun', obs, -1, date, 300))
 
 
 let planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Moon'];
 
-let riseTimes = planets.map(planet => {
-  return Astronomy.SearchRiseSet(planet, obs, +1, yesterday, 300).date.toLocaleString("en-US", {
-    timeZone: "America/Los_Angeles"
+let planetRiseSetTimes = planets.map(planet => {
+  return {
+    planet, 
+    riseTimes: new Date(Astronomy.SearchRiseSet(planet, obs, +1, yesterday, 300)), 
+    setTimes: new Date(Astronomy.SearchRiseSet(planet, obs, -1, date, 300))
+  }
+});
+
+function filterBySetTimes(planetData){
+  return planetData.filter(obj => {
+    return obj.setTimes.getTime() > sunSetTime.getTime();
   })
-})
+}
 
-let setTimes = planets.map(planet => {
-  return Astronomy.SearchRiseSet(planet, obs, -1, date, 300).date.toLocaleString("en-US", {
-    timeZone: "America/Los_Angeles"
+function filterByRiseTimes(planetData){
+  return planetData.filter(obj => {
+    return obj.riseTimes.getTime() < sunRiseTime.getTime();
   })
-})
+}
 
 
-riseTimes.forEach(ele => console.log(ele))
-console.log('-------------------')
-setTimes.forEach(ele => console.log(ele))
-console.log('sun rise time' + ' ' + sunSetTime);
-console.log('sun set time' + ' ' + sunRiseTime)
+console.log('Set:', filterBySetTimes(planetRiseSetTimes), '\n')
+console.log('Rise:', filterByRiseTimes(planetRiseSetTimes))
+console.log('Sunrise time: ', sunRiseTime, 'sunset time: ', sunSetTime)
 
-console.log(riseTimes[0] < sunSetTime);
+// .toLocaleString("en-US", {timeZone: "America/Los_Angeles"})
+// let setTimes = planets.map(planet => {
+//   return {planet, setTimes: new Date(Astronomy.SearchRiseSet(planet, obs, -1, date, 300))}
+// });
+
+
+// now they are type object
+
+// let testing1 = setTimes.map(ele => {
+//   if (ele.getTime() < sunSetTime.getTime()) {
+//     return 'n/a'
+//   }
+//   return ele;
+// })
+
+// let testing = setTimes.filter((obj)=>{
+//   return obj.setTimes.getTime() > sunSetTime.getTime()
+// })
+
+// riseTimes.map(ele => {
+//   if (ele > sunRiseTime) {
+//     return 'n/a'
+//   } 
+//   return ele;
+  
+// })
+
+// console.log(testing)
+// testing.forEach(ele => console.log(ele));
+// console.log('-------------------');
+// setTimes.forEach(ele => console.log(ele));
+// console.log(sunSetTime.toLocaleString("en-US", {
+//   timeZone: "America/Los_Angeles"
+// }));
+// console.log(sunRiseTime);
+
+// sunSetTime.date.toLocaleString("en-US", {
+//   timeZone: "America/Los_Angeles"
+// });]
