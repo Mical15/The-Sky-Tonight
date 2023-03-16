f1cec7fe753fb7e8c265cae5ec75bd193969278b
Michael Gallien <39108801+Mical15@users.noreply.github.com>
2023-03-15 17:32:30 -0700

    Finished Logic!

--- ASTR TESTING/testing.js
+++ ASTR TESTING/testing.js
@@ -1,8 +1,5 @@
 const Astronomy = require('/Users/michaelgallien/Projects/astrLocations/The-Sky-Tonight/astronomy.js');
 
-// import {Observer, SearchRiseSet} from '/Users/michaelgallien/Projects/astrLocations/The-Sky-Tonight/astronomy.js';
-
-
 const location = {
   lat: 37.5355,
   lon: -122.3355
@@ -13,17 +10,15 @@ let yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 const date = new Date();
 
-// .toLocaleString("en-US", {
-//   timeZone: "America/Los_Angeles"
-// })
-
 let test = Astronomy.SearchRiseSet('Sun', obs, +1, date, 300);
 
+let todaysSunRiseTime = new Date(Astronomy.SearchRiseSet('Sun', obs, +1, yesterday, 300))
 let sunRiseTime = new Date(Astronomy.SearchRiseSet('Sun', obs, +1, date, 300))
-let sunSetTime = new Date(Astronomy.SearchRiseSet('Sun', obs, -1, date, 300))
+let sunSetTime = new Date(Astronomy.SearchRiseSet('Sun', obs, -1, date, 300)) // need to test, I think the library changes "date" once sunset time has been reached. Useful for astronomeres, not useful for people.
+// let sunSetTime = new Date().now() <= new Date(Astronomy.SearchRiseSet('Sun', obs, -1, date, 300)).getTime() ? new Date(Astronomy.SearchRiseSet('Sun', obs, -1, date, 300)) : new Date(Astronomy.SearchRiseSet('Sun', obs, -1, yesterday, 300))
 
 
-let planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Moon'];
+let planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']; // add moon last, it's always visable when above horizon; day or night
 
 let planetRiseSetTimes = planets.map(planet => {
   return {
@@ -33,59 +28,33 @@ let planetRiseSetTimes = planets.map(planet => {
   }
 });
 
-function filterBySetTimes(planetData){
+function filterByRiseSetTimes (planetData) {
   return planetData.filter(obj => {
-    return obj.setTimes.getTime() > sunSetTime.getTime();
+    return !(obj.setTimes.getTime() < sunSetTime.getTime() && obj.riseTimes.getTime() > todaysSunRiseTime.getTime());
   })
 }
 
-function filterByRiseTimes(planetData){
-  return planetData.filter(obj => {
-    return obj.riseTimes.getTime() < sunRiseTime.getTime();
-  })
-}
+let filteredPlanetTimes = filterByRiseSetTimes(planetRiseSetTimes);
+filteredPlanetTimes.forEach( ele => {
+  if (ele.riseTimes.getTime() > todaysSunRiseTime.getTime()) {
+    ele.riseTimes = 'Sunset';
+  }
+})
 
+filteredPlanetTimes.forEach( ele => { 
+  if (ele.setTimes.getTime() < sunSetTime.getTime()) {
+    ele.setTimes = 'Sunrise'
+  }
+})
 
-console.log('Set:', filterBySetTimes(planetRiseSetTimes), '\n')
-console.log('Rise:', filterByRiseTimes(planetRiseSetTimes))
-console.log('Sunrise time: ', sunRiseTime, 'sunset time: ', sunSetTime)
+filteredPlanetTimes.push({
+  planet: 'Moon', 
+  riseTimes: new Date(Astronomy.SearchRiseSet('Moon', obs, +1, yesterday, 300)), 
+  setTimes: new Date(Astronomy.SearchRiseSet('Moon', obs, -1, date, 300))
+})
 
-// .toLocaleString("en-US", {timeZone: "America/Los_Angeles"})
-// let setTimes = planets.map(planet => {
-//   return {planet, setTimes: new Date(Astronomy.SearchRiseSet(planet, obs, -1, date, 300))}
-// });
+filteredPlanetTimes.forEach(ele => {
+  console.log(ele.planet, 'Rise Time: ', ele.riseTimes.toLocaleString("en-US", {timeZone: "America/Los_Angeles"}), 'Set Time: ', ele.setTimes.toLocaleString("en-US", {timeZone: "America/Los_Angeles"}))
+})
 
-
-// now they are type object
-
-// let testing1 = setTimes.map(ele => {
-//   if (ele.getTime() < sunSetTime.getTime()) {
-//     return 'n/a'
-//   }
-//   return ele;
-// })
-
-// let testing = setTimes.filter((obj)=>{
-//   return obj.setTimes.getTime() > sunSetTime.getTime()
-// })
-
-// riseTimes.map(ele => {
-//   if (ele > sunRiseTime) {
-//     return 'n/a'
-//   } 
-//   return ele;
-  
-// })
-
-// console.log(testing)
-// testing.forEach(ele => console.log(ele));
-// console.log('-------------------');
-// setTimes.forEach(ele => console.log(ele));
-// console.log(sunSetTime.toLocaleString("en-US", {
-//   timeZone: "America/Los_Angeles"
-// }));
-// console.log(sunRiseTime);
-
-// sunSetTime.date.toLocaleString("en-US", {
-//   timeZone: "America/Los_Angeles"
-// });]
+console.log('\n', 'Sunrise time: ', sunRiseTime.toLocaleString("en-US", {timeZone: "America/Los_Angeles"}), 'sunset time: ', sunSetTime.toLocaleString("en-US", {timeZone: "America/Los_Angeles"}), '\n')
